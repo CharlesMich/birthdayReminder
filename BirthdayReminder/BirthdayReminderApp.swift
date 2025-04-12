62a7c8a88881c493dc73b93282ad36080344205b
CharlesMich <charleedongo@hotmail.com>
2025-04-12 17:15:09 -0500

    Initial Commit

---
+++ BirthdayReminder/BirthdayReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayReminderApp.swift
+//  BirthdayReminder
+//
+//  Created by Charles Michael on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
